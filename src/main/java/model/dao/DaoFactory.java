656fdf43407e41202a8d4a3bc8597b7e8fc60fa1
Gabriella <gabriellalok3@gmail.com>
2023-03-01 21:31:31 -0300

    implementando SellerDao e DaoFactory

---
+++ src/main/java/model/dao/DaoFactory.java
@@ -0,0 +1,13 @@
+package model.dao;
+
+import model.dao.model.dao.impl.SellerDaoJDBC;
+
+//classe auxiliar responsavel por instanciar os DAOs
+public class DaoFactory {
+
+    //macete - desta forma a minha classe vai expor um metodo que retorna o tipo da interface SellerDao
+    //mas internamente vai instanciar uma implementacao e desta forma não expoe a implementacao
+    public static SellerDao createSellerDao(){
+            return new SellerDaoJDBC();
+    }
+}
