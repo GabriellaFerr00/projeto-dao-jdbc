3a85783fbde0e42bd4e6688785762815b29c9992
Gabriella <gabriellalok3@gmail.com>
2023-03-02 02:46:18 -0300

    implementando operacao delete

--- src/main/java/model/dao/model/dao/impl/SellerDaoJDBC.java
+++ src/main/java/model/dao/model/dao/impl/SellerDaoJDBC.java
@@ -58,12 +58,52 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void update(Seller seller) {
+        PreparedStatement st = null;
+        try{
+            st = conn.prepareStatement(
+                    "UPDATE seller "
+                            + "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+                            + "WHERE id = ? ",
+                    Statement.RETURN_GENERATED_KEYS
+            );
+
+            st.setString(1, seller.getName());
+            st.setString(2, seller.getEmail());
+            st.setDate(3, new java.sql.Date(seller.getBirthDate().getTime()));
+            st.setDouble(4, seller.getBaseSalary());
+            st.setInt(5, seller.getDepartment().getId());
+            st.setInt(6, seller.getId());
+
+           st.executeUpdate();
+
+        } catch (SQLException e){
+            throw new DbException(e.getMessage());
+        } finally {
+            ConnectionDb.closeStatement(st);
+        }
 
     }
 
     @Override
     public void deleteById(Integer id) {
+        PreparedStatement st = null;
 
+        try {
+            st = conn.prepareStatement(
+                    "DELETE FROM seller WHERE id = ?"
+                    );
+            st.setInt(1, id);
+
+            int rows = st.executeUpdate();
+
+            if(rows == 0){
+                throw new DbException("Id does not exist in database");
+            }
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            ConnectionDb.closeStatement(st);
+        }
     }
 
     @Override
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,9 +7,11 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
@@ -35,5 +37,18 @@ public class Main {
         sellerDao.insert(sellerInsert);
         System.out.println("Inserted - New id: " + sellerInsert.getId());
 
+        System.out.println("\n************ TEST 5: Seller update ************");
+        seller = sellerDao.findById(1);
+        seller.setName("Maria joaquina");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
+
+        System.out.println("\n************ TEST 6: Seller delete ************");
+        System.out.print("Enter id for delete: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed");
+
+        sc.close();
     }
 }
